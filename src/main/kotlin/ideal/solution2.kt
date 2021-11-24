4c3b657c406053cad94dd33eac06070aac963194
Alexander Maslyaev <maslyaevalexandr@gmail.com>
2021-11-24 14:47:31 +0300

    Update constants

--- src/main/kotlin/ideal/solution2.kt
+++ src/main/kotlin/ideal/solution2.kt
@@ -3,9 +3,9 @@ package ideal
 import kotlin.math.abs
 import kotlin.math.exp
 
-fun funResult(value: Double): Double = 35 - 0.0025 * (exp(value / 0.038) - 1) - 0.06173 * value * exp(value / 0.038)
+fun funResult(value: Double): Double = 35 - 0.0025 * (exp(value / 0.0385) - 1) - 0.06173 * value * exp(value / 0.0385)
 
-fun difResult(value: Double): Double = -0.0526 * (exp(value / 0.038)) - 1.62447 * value * exp(value / 0.038)
+fun difResult(value: Double): Double = -0.0526 * (exp(value / 0.0385)) - 1.62447 * value * exp(value / 0.0385)
 
 fun gFunResult(value: Double): Double = value - funResult(value) / difResult(value)
 
@@ -20,11 +20,11 @@ fun main() {
         var count = 0L
         while (eps < abs(funResult(value = root!!))) {
             if (difResult(root) == 0.0) break
-            print("$count  ")
-            print("$root  ")
-            print("${gFunResult(root)}  ")
-            print("${difResult(root)}  ")
-            print("${funResult(root)}  ")
+            print("Iteration = $count  ")
+            print("Root = $root  ")
+            print("g(x) = ${gFunResult(root)}  ")
+            print("df(x) = ${difResult(root)}  ")
+            print("f(x) = ${funResult(root)}  ")
             root = gFunResult(root)
             count++
             println()
